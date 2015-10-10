85d3e509a06b486725080c91cf16d1f325aa6c50
Anubhav Maity <anubhavmaity@Anubhavs-MacBook-Air.local>
2015-10-10 15:31:08 +0530

    introduce position of the data

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var checkForFloat = function(numeric) {
+	var numeric = parseInt(numeric);
+	return n === Number(n) && n % 1 !== 0;
+}
+var checkForInt = function(numeric) {
+	var numeric = parseInt(numeric);
+	return Number(n) === n && n % 1 === 0;
+}
+//checks for string of alphabets and numbers
+var checkForProductName = function(prod_name) {
+	var re = /[a-zA-Z\d-,\/]+/;
+	return re.test(prod_name);
+}
+
 var checkForCustomerName = function(name) {
 	var re = /^[a-zA-Z ]{2,30}$/;
 	return re.test(name);
@@ -6,11 +20,6 @@ var checkForCustomerName = function(name) {
 var checkForAddress = function(address) {
 	var re = /^[a-zA-Z0-9\s,'-]*$/;
 	return re.test(address);
-}
-
-var checkForProductName = function(prod_name) {
-	var re = /[a-zA-Z\d-,\/]+/;
-	return re.test(prod_name);
 }
 
 var checkForProdQuant = function(prod_quant) {
@@ -34,6 +43,11 @@ var checkForInvoiceNo = function(invoice_no) {
 
 }
 
+var findUnitPriceUnitQuanity = function() {
+	//will require postion
+	//other than that getting difficult to get the data, it is taking lot of time to multiply everything
+	//optimize it
+}
 
 var checkForCurrencySymbol = function(price) {
 	//have to recheck the code
@@ -74,12 +88,26 @@ var checkForPlaceInAddr = function(addr) {
 
 
 //shift this to script.js
-var arr = ['External Brand elements as per the agreed designs and specifications', 'External brand prints as per the agreed designs and specifications', 
-'Floor Directory as per the agreed designs and specifications', 'Design/ Agency  Charges', 'Packaging, Loading, Unloading, Installation Charges', 
-'2', '3', '4', '5' , '2', '1', '2', '₹ 100.00', '₹ 200.00', '₹ 300.00', '₹ 200.00', '₹ 200.00', '₹ 600.00', '₹ 10.00', '₹ 20.00', '₹ 1,030.00', '₹ 23,540.00', '-₹ 2,817.50', 
-'₹ 3.71', '₹ 28,000.00', '₹ 2,676.21', 'Apr 09, 2015', 'CABS/INV/15-16/MV/001', 'CABS/EST/14-15/MV/048', 'XXX/NO/PROJ/PO/2014-15/10/025', 'Mr Amod Jha', 'XXX YYT New Delhi']
+// var arr = [{'field': 'External Brand elements as per the agreed designs and specifications', 'position': 'upper left'}, 
+// {'field':'External brand prints as per the agreed designs and specifications', 'position': 'upper right'}, 
+// {'field': 'Floor Directory as per the agreed designs and specifications', 'Design/ Agency  Charges', 'Packaging, Loading, Unloading, Installation Charges', 
+// '2', '3', '4', '5' , '2', '1', '2', '₹ 100.00', '₹ 200.00', '₹ 300.00', '₹ 200.00', '₹ 200.00', '₹ 600.00', '₹ 10.00', '₹ 20.00', '₹ 1,030.00', '₹ 23,540.00', '-₹ 2,817.50', 
+// '₹ 3.71', '₹ 28,000.00', '₹ 2,676.21', 'Apr 09, 2015', 'CABS/INV/15-16/MV/001', 'CABS/EST/14-15/MV/048', 'XXX/NO/PROJ/PO/2014-15/10/025', 'Mr Amod Jha', 'XXX YYT New Delhi']
 
-
+var arr = [{'data':'Mr Amod Jha, XXX, YYT, New Delhi', 'position': '3A'}, {'data':'Apr 09, 2015', 'position':'3E'}, 
+{'data': 'CABS/INV/15-16/MV/001', 'position':'4E'}, {'data': 'CABS/EST/14-15/MV/048', 'position': '5E'}, 
+{'data': 'XXX/NO/PROJ/PO/2014-15/10/025', 'position': '6E'}, {'data': 'Mr Amod Jha', 'position': '7E'}, {'data': '1', 'position':'14A'}, 
+{'data': 'External Brand elements as per the agreed designs and specifications', 'position':'14BC'},
+{'data': '2', 'position':'14D'}, {'data': '₹ 100.00', 'position':'14E'}, {'data': '₹ 200.00', 'position':'14F'}, {'data': '2', 'position':'15A'},
+{'data': 'External brand prints as per the agreed designs and specifications	', 'position':'15BC'}, {'data': '1', 'position':'15D'}, 
+{'data': '₹ 200.00', 'position':'15E'}, {'data': '₹ 200.00', 'position':'15F'},
+{'data': '3', 'position':'16A'}, {'data': 'Floor Directory as per the agreed designs and specifications	', 'position':'16BC'}, 
+{'data': '2', 'position':'16D'}, {'data': '₹ 300.00', 'position':'16E'}, {'data': '₹ 600.00', 'position':'16F'}
+{'data': '4', 'position': '17A'}, {'data': 'Design/ Agency  Charges', 'position':'17BC'}, {'data': '₹ 10.00', 'position':'17F'},
+{'data': '5', 'position': '18A'}, {'data': 'Packaging, Loading, Unloading, Installation Charges', 'position':'18BC'}, 
+{'data': '₹ 20.00', 'position':'18F'}, {'data': '₹ 1,030.00', 'position': '22F'}, {'data': '₹ 23,540.00', 'position': '23F'},
+{'data': '-₹ 2,817.50', 'position': '24F'}, {'data': '₹ 3.71', 'position': '25F'}, {'data': '₹ 28,000.00', 'position': '26F'},
+{'data': '₹ 2,676.21', 'position': '27F'}]
 
 var classifyFields = function(arr) {
 	var output_arr = [];
@@ -98,7 +126,7 @@ var classifyFields = function(arr) {
 		if (checkForAddress(arr[i]) && checkForPlaceInAddr(arr[i])) {
 			//console.log("hi");
 			//console.log(checkForAddress(arr[i]));
-			//console.log(checkForPlaceInAddr(arr[i]));
+			//console.log(checkForP,laceInAddr(arr[i]));
 			fields_dict["category"].push("address");
 		}
 		if (checkForCustomerName(arr[i])) {
@@ -107,7 +135,7 @@ var classifyFields = function(arr) {
 		}
 		
 		if (checkForProdQuant(arr[i])) {
-			console.log(arr[i] + " is product quantity");
+
 			fields_dict["category"].push("product quantity");
 		}
 		if (checkForPrices(arr[i]) || checkForCurrencySymbol(arr[i])) {
